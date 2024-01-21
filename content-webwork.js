38988fc6494f472b8bdaeeb45073a866cf684c5c
crimson-dynamo <156981781+crimson-dynamo@users.noreply.github.com>
2024-01-20 18:35:00 -0800

    Update content-webwork.js
    Fixed bug with multiple clear answer buttons showing up after turning on and off extension

--- content-webwork.js
+++ content-webwork.js
@@ -8,7 +8,6 @@ var webworkSetup = function () {
     }
 
     var retrieveTextInputs = function () {
-        // Get all answer fields (they seem to all have the CSS class name "codeshard")
         return document.getElementsByClassName("codeshard");
     }
 
@@ -16,34 +15,28 @@ var webworkSetup = function () {
         return document.getElementsByClassName("pg-select");
     }
 
-    var applyToInputs = function (inputs) {
+    var applyToInputs = function () {
         console.log("[WeBWorK MathView] Inserting MathView elements");
         var inputs = retrieveTextInputs();
         for (var i = 0; i < inputs.length; i++) {
             var theInput = inputs[i];
 
-            // Only attach a MathView if one hasn't already been attached
             if (!MathView.hasMathView(theInput)) {
-                /********* Attach MathView *********/
                 var aMath = theInput.value;
-        
+
                 var mathView = MathView.createMathView(i, theInput);
-        
-                // Insert the math containers directly after the text input
-		theInput.nextSibling.insertAdjacentElement('afterend', mathView);
+
+                theInput.nextSibling.insertAdjacentElement('afterend', mathView);
                 MathView.updateMath(i, aMath);
 
-                /********* Disable spell check *********/
                 theInput.setAttribute("autocomplete", "off");
                 theInput.setAttribute("autocorrect", "off");
                 theInput.setAttribute("autocapitalize", "off");
                 theInput.setAttribute("spellcheck", "false");
 
-                /********* Custom font *********/
                 theInput.style.fontSize = MATH_FONT.size;
                 theInput.style.fontFamily = MATH_FONT.family;
             }
-
         }
         console.log("[WeBWorK MathView] Rendered");
     }
@@ -51,17 +44,22 @@ var webworkSetup = function () {
     var createClearAnswers = function () {
         console.log("[WeBWorK MathView] Creating Clear Answers button");
 
-        // We'll be inserting our button next to previewAnswers
+        // Check if the button already exists
+        if (document.getElementById("clearAnswersButton")) {
+            console.log("[WeBWorK MathView] Clear Answers button already attached");
+            return;
+        }
+
         var previewAnswers = document.getElementById("previewAnswers_id");
         if (previewAnswers != null) {
-            // Create the button
             var clearAnswers = document.createElement("input");
+            clearAnswers.id = "clearAnswersButton";
             clearAnswers.className = "btn btn-primary mb-1";
             clearAnswers.type = "submit";
             clearAnswers.value = "Clear Answers";
             clearAnswers.style.backgroundColor = "#dd5555";
             clearAnswers.style.backgroundImage = "none";
-            // Attach onclick listener
+
             clearAnswers.addEventListener("click", function (e) {
                 e.preventDefault();
                 if (!confirm("Are you sure you want to clear all answer boxes on this page. This cannot be undone.")) {
@@ -83,7 +81,7 @@ var webworkSetup = function () {
                     selectInputs[i].value = 0;
                 }
             });
-            // Insert the button
+
             previewAnswers.parentNode.insertBefore(clearAnswers, null);
         }
     }
@@ -97,7 +95,6 @@ var webworkSetup = function () {
     var selectInputs = retrieveSelectInputs();
 
     if (textInputs.length == 0 && selectInputs.length == 0) {
-        // If no inputs are found, wait until the DOMContentLoaded event fires to search for them
         console.log("[WeBWorK MathView] DOM not available. Waiting to insert MathView elements...");
         document.addEventListener("DOMContentLoaded", function () {
             console.log("[WeBWorK MathView] DOM available");
@@ -105,7 +102,6 @@ var webworkSetup = function () {
         })
     }
     else {
-        // If we found inputs go ahead and attach MathViews to them immediately
         main();
     }
 };
