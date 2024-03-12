7daac170c2b0a800a8049473ea93c4d81a29048c
Daniel Benjamin <156981781+danielrbenjamin@users.noreply.github.com>
2024-03-11 17:26:27 -0700

    Update wwscript.js
    Fixed errors with fixing the overlay in place.

--- wwscript.js
+++ wwscript.js
@@ -1,9 +1,115 @@
+// Adapted by Daniel Benjamin
+// from
 // wwchecker
 // james yuzawa
 // jyuzawa.com
 // github.com/yuzawa-san
 
 var checkerActive = false;
+var floater, prefBox;
+
+function spaces(n) {
+    var str = "";
+    for (var i = 0; i < n; i++) {
+        str += "&nbsp;";
+    }
+    return str;
+}
+
+function isValid($obj) {
+    var expr = $obj.val();
+    var floater = $("#wwchecker-floater");
+
+    if (checkerActive && expr !== '') {
+        // Validation logic for matching parentheses
+        var validationOutput = checkMatchingBrackets(expr);
+
+        if (validationOutput[0]) {
+            floater.show();
+            showValidFloater(floater, validationOutput[2]);
+        } else {
+            floater.show();
+            showInvalidFloater(floater, validationOutput[2], validationOutput[1]);
+        }
+    } else {
+        floater.hide();
+    }
+}
+
+function checkMatchingBrackets(expr) {
+    var tokenStack = [];
+    var sexyText = "";
+    var ct = 0;
+    var lastUnclosedIndex = -1;
+
+    for (var i = 0; i < expr.length; i++) {
+        var ch = expr.charAt(i);
+
+        if (ch == "(" || ch == "[" || ch == "{") {
+            // opening grouping
+            tokenStack.push(ch);
+            sexyText += "<span class=ww_pair" + (ct % 5) + ">" + ch + "</span>";
+            ct++;
+        } else if (ch == ")" || ch == "]" || ch == "}") {
+            // closing grouping
+            var head = tokenStack[tokenStack.length - 1];
+            if (!head) {
+                // stack is empty
+                return [false, i, sexyText + "<span class=ww_pairbad>" + expr.substring(i) + "</span>"];
+            }
+            if ((head == "(" && ch == ")") || (head == "[" && ch == "]") || (head == "{" && ch == "}")) {
+                // match found
+                tokenStack.pop();
+                ct--;
+                sexyText += "<span class=ww_pair" + (ct % 5) + ">" + ch + "</span>";
+            } else {
+                // grouping mismatch
+                lastUnclosedIndex = i;
+                return [false, lastUnclosedIndex, sexyText];
+            }
+        } else {
+            // other characters, escaping html
+            if (ch == "<") {
+                sexyText += "&lt;";
+            } else if (ct == ">") {
+                sexyText += "&gt;";
+            } else {
+                sexyText += ch;
+            }
+        }
+    }
+
+    if (tokenStack.length > 0) {
+        // Unclosed brackets at the end
+        lastUnclosedIndex = expr.length - 1;
+    }
+
+    return [tokenStack.length === 0, lastUnclosedIndex, sexyText];
+}
+
+function showValidFloater(floater, content) {
+    floater.removeClass('ww_invalid_floater');
+    floater.html(content);
+}
+
+function showInvalidFloater(floater, content, errorLocation) {
+    floater.addClass('ww_invalid_floater');
+    floater.html(content + "<BR>" + spaces(errorLocation) + "<span class=ww_fail>^</span>");
+}
+
+function updateFloaterPosition(inputElement) {
+    var floater = $("#wwchecker-floater");
+
+    if (inputElement) {
+        var offsetStruct = inputElement.offset();
+        var itemHeight = inputElement.outerHeight();
+
+        floater.css({
+            left: offsetStruct.left,
+            top: offsetStruct.top + itemHeight + 10
+        });
+    }
+}
 
 $(document).ready(function () {
     // make floater box
@@ -26,28 +132,19 @@ $(document).ready(function () {
 
     // bind events to answer boxes
     $('input[id^=AnSwEr], input[id^=MuLtIaNsWeR]').each(function () {
-        // do initial check on all of the boxes
-        isValid($(this));
-    }).keyup(function () {
-        // eval on keyup
-        isValid($(this));
-    }).focus(function (event) {
-        // display floater on focus
-        if (!checkerActive) {
-            return;
-        }
-        // setup metrics
-        offsetStruct = $(this).offset();
-        itemWidth = $(this).outerWidth();
-        itemHeight = $(this).outerHeight();
-        $("#wwchecker-floater").css({
-            left: offsetStruct.left,
-            top: offsetStruct.top + itemHeight + 10
-        }).show();
-        isValid($(this));
-    }).blur(function () {
-        // hide floater on blur
-        $("#wwchecker-floater").hide();
+        var inputElement = $(this);
+
+        inputElement.on('input', function () {
+            isValid(inputElement);
+        });
+
+        inputElement.focus(function () {
+            updateFloaterPosition(inputElement);
+        });
+
+        inputElement.blur(function () {
+            floater.hide();
+        });
     });
 
     // bind preferences
@@ -59,91 +156,29 @@ $(document).ready(function () {
             localStorage.wwactive = 'inactive';
         }
     });
+
+    // reposition floater on window resize
+    $(window).resize(function () {
+        var focusedInput = $('input:focus');
+        if (focusedInput.length > 0) {
+            updateFloaterPosition(focusedInput);
+        }
+    });
+
+    // reposition floater on window scroll
+    $(window).scroll(function () {
+        var focusedInput = $('input:focus');
+        if (focusedInput.length > 0) {
+            updateFloaterPosition(focusedInput);
+        }
+    });
 });
 
-// generate proper number of spaces
-function spaces(n) {
-    var str = "";
-    for (var i = 0; i < n; i++) {
-        str += "&nbsp;";
+// validate input box on page load
+$(window).load(function () {
+    var focusedInput = $('input:focus');
+    if (focusedInput.length > 0) {
+        isValid(focusedInput);
+        updateFloaterPosition(focusedInput);
     }
-    return str;
-}
-
-// check validation of the value of an object.
-function isValid($obj) {
-    var expr = $obj.val();
-    // display box only if preferences allow, string is nonempty, and contains grouping characters
-    if (checkerActive && expr !== '' && expr.match(/[\(\[\{\)\]\}]/)) {
-        $("#wwchecker-floater").show();
-    } else {
-        $("#wwchecker-floater").hide();
-    }
-    // validation returns: [valid, error location, formatted string]
-    var validationOutput = function (str) {
-        var tokenStack = [];
-        var sexyText = "";
-        var ct = 0;
-        for (var i = 0; i < str.length; i++) {
-            ch = str.charAt(i);
-            if (ch == "(" || ch == "[" || ch == "{") {
-                // opening grouping
-                tokenStack.push(ch);
-                sexyText += "<span class=ww_pair" + (ct % 5) + ">" + ch + "</span>";
-                ct++;
-            } else if (ch == ")" || ch == "]" || ch == "}") {
-                // closing grouping
-                head = tokenStack[tokenStack.length - 1];
-                if (!head) {
-                    // stack is empty
-                    return [false, i, sexyText + "<span class=ww_pairbad>" + str.substring(i) + "</span>"];
-                }
-                if ((head == "(" && ch == ")") || (head == "[" && ch == "]") || (head == "{" && ch == "}")) {
-                    // match found
-                    tokenStack.pop();
-                    ct--;
-                    sexyText += "<span class=ww_pair" + (ct % 5) + ">" + ch + "</span>";
-                } else {
-                    // grouping mistmatch
-                    return [false, i, sexyText + "<span class=ww_pairbad>" + str.substring(i) + "</span>"];
-                }
-            } else {
-                // other characters, escaping html
-                if (ch == "<") {
-                    sexyText += "&lt;";
-                } else if (ct == ">") {
-                    sexyText += "&gt;";
-                } else {
-                    sexyText += ch;
-                }
-            }
-        }
-        if (tokenStack.length === 0) {
-            // valid
-            return [true, 0, sexyText];
-        }
-        // invalid at end
-        return [false, str.length - 1, sexyText];
-    }(expr);
-
-    // remove any old styles
-    if ($obj.attr("style")) {
-        $obj.removeAttr("style");
-    }
-
-    var floater = $("#wwchecker-floater");
-
-    if (validationOutput[0]) {
-        // is valid
-        $obj.addClass('ww_valid');
-        $obj.removeClass('ww_invalid');
-        floater.html(validationOutput[2]);
-        floater.removeClass('ww_invalid_floater'); // Remove invalid floater class
-    } else {
-        // is invalid, render error location
-        $obj.addClass('ww_invalid');
-        $obj.removeClass('ww_valid');
-        floater.html(validationOutput[2] + "<BR>" + spaces(validationOutput[1]) + "<span class=ww_fail>^</span>");
-        floater.addClass('ww_invalid_floater'); // Add invalid floater class
-    }
-}
+});
