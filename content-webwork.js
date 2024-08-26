04e3e9687f0352e304f12ebc9bae4140b2d4f709
Daniel Benjamin <156981781+danielrbenjamin@users.noreply.github.com>
2024-08-25 18:40:45 -0700

    previous uncommited revision 1.4.2 which added toggles and various features, but did not fix DOM issue

--- content-webwork.js
+++ content-webwork.js
@@ -184,11 +184,11 @@ var webworkSetup = async function () {
         await createResourcesButton();
     };
 
-    // Check if the "problem-content" div is available instead of codeshard or pg-select classes
-    var problemContent = document.querySelector(".problem-content");
+    var textInputs = retrieveTextInputs();
+    var selectInputs = retrieveSelectInputs();
 
-    if (!problemContent) {
-        console.log("[WeBWorKer] 'problem-content' not available. Waiting to insert MathView elements...");
+    if (textInputs.length == 0 && selectInputs.length == 0) {
+        console.log("[WeBWorKer] DOM not available. Waiting to insert MathView elements...");
         document.addEventListener("DOMContentLoaded", function () {
             console.log("[WeBWorKer] DOM available");
             main();
